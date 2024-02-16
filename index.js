f62b7712a60453be8f57b0f825c2d5d93d4d6dc6
Betelgeuse <abdumavlyanov2004@yahoo.com>
2024-02-16 20:45:38 +0000

    updated with inquirer code

--- index.js
+++ index.js
@@ -4,16 +4,70 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please give brief description about the project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please enter usage information',
+    },
+    {
+      type: 'list',
+      name: 'Licence',
+      message: 'Please select a Licence',
+      choices: ['MIT', 'GPL', 'Apache', 'None']
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please enter contributing guidelines',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email',
+    },
+  ]);
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+promptUser().then(data => {
+    console.log(data)
+    let RedMe = generateMarkdown(data)
+    console.log(RedMe)
+})
+
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let badge;
+  if(data.licence === 'MIT') {
+    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  
   return `# ${data.title}
-
+  ${badge}
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Licence
+${data.licence}
+## Contributing
+${data.contributing}
+## Questions
+https://github.com/${data.github}
+You can reach out to me at ${data.email}
 `;
 }
 
